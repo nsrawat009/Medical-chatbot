aa76e201e285a58b3c4125dfec6347959e2a1ad5
nsrawat009 <rawatnarendra009@gmail.com>
2024-01-20 18:30:56 +0530

    trials python added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'narendra rawat',
+    author_email= 'rawatnarenddra009@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
